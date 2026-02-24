a6991a84e1f77863a6169455d13bce024b64d29e
heizrockz <jamboristore@gmail.com>
2026-02-24 13:33:24 +0400

    Update XYZ

--- views/user/partials/navbar.php
+++ views/user/partials/navbar.php
@@ -3,17 +3,17 @@
     <span class="nav-logo">⚡ <?= e(getSetting('site_name', APP_NAME)) ?></span>
 
     <ul class="nav-links">
-        <li><a href="#hero"><?= t('nav_home') ?></a></li>
-        <li><a href="#services"><?= t('nav_services') ?></a></li>
-        <li><a href="#about"><?= t('nav_about') ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#hero"><?= t('nav_home') ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#services"><?= t('nav_services') ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#about"><?= t('nav_about') ?></a></li>
         <li><a href="<?= baseUrl('portfolio') ?>"><?= t('nav_portfolio') ?></a></li>
         <?php if(getSetting('show_products_section', '1') === '1'): ?>
-        <li><a href="#products"><?= getCurrentLocale() === 'en' ? 'Products' : 'منتجات' ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#products"><?= getCurrentLocale() === 'en' ? 'Products' : 'منتجات' ?></a></li>
         <?php endif; ?>
         <?php if(getSetting('show_clients_section', '1') === '1'): ?>
-        <li><a href="#clients"><?= getCurrentLocale() === 'en' ? 'Clients' : 'عملاء' ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#clients"><?= getCurrentLocale() === 'en' ? 'Clients' : 'عملاء' ?></a></li>
         <?php endif; ?>
-        <li><a href="#booking" class="nav-cta-btn"><?= t('nav_booking') ?></a></li>
+        <li><a href="<?= baseUrl('/') ?>#booking" class="nav-cta-btn"><?= t('nav_booking') ?></a></li>
     </ul>
 
     <a href="<?= baseUrl('?lang=' . (getCurrentLocale() === 'en' ? 'ar' : 'en')) ?>" class="nav-lang-btn">
